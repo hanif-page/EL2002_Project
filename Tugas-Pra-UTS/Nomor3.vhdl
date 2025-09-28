8e8713b196d942d41811f5aa83e97dfa0d11ae21
hanif-page <amrhanif15@gmail.com>
2025-09-29 04:13:26 +0700

    feat: Add the B 2's complements calculation before it's getting operated with the A bit

--- Tugas-Pra-UTS/Nomor3.vhdl
+++ Tugas-Pra-UTS/Nomor3.vhdl
@@ -1,21 +1,28 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity rangkaian_ED is 
     port (
         A : in std_logic_vector(3 downto 0);
         B : in std_logic_vector(3 downto 0);
+        -- D : out std_logic_vector(3 downto 0);
         A0 : out std_logic;
-        B0 : out std_logic;
+        D0 : out std_logic;
         X : out std_logic
     );
 end entity rangkaian_ED;
 
 architecture behavioral of rangkaian_ED is 
+    signal B_negative : std_logic_vector(3 downto 0);
 begin
+    -- getting the B 2's complements
+    B_negative <= std_logic_vector(unsigned(not B) + 1); 
+    
     A0 <= A(0);
-    B0 <= B(0);
+    -- D <= B_negative;
+    D0 <= B_negative(0);
 
-    X <= not(A0 xor B0);
+    X <= not(A0 xor D0);
     
 end architecture behavioral
